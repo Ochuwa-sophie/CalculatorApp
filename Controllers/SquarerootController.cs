000dc2e11faf820a982380b54fdc2ea31f4d869c
Ochuwa-sophie <sophiecodes@gmail.com>
2020-05-07 08:09:45 +0100

    First commit

---
+++ Controllers/SquarerootController.cs
@@ -0,0 +1,49 @@
+using System.ComponentModel;
+using System;
+using Microsoft.AspNetCore.Mvc;
+
+namespace squarerootWebApp.Controllers
+{
+    public class SquarerootController : Controller
+    {
+        [HttpGet]
+        public IActionResult Squareroot()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Squareroot(string FirstNumber, string SecondNumber)
+        {
+            double NumberOne = Math.Sqrt(double.Parse(FirstNumber));
+            double numberTwo = Math.Sqrt(double.Parse(SecondNumber));
+            
+            ViewBag.First =  double.Parse(FirstNumber);
+            ViewBag.Second = double.Parse(SecondNumber);                   
+            ViewBag.One = NumberOne;
+            ViewBag.Two = NumberTwo;    
+            
+
+        /*    if (NumberOne < 0)
+            {
+                
+                       
+                            Input positive numbers only.
+            }
+             break;
+
+    
+            if (NumberTwo < 0)
+            {
+               
+                            Input positive numbers only.
+                       
+            }
+            break;
+
+    }*/
+
+            return View();
+        }
+    }
+}
